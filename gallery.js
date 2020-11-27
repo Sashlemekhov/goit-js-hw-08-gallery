c1fa8043692687cf3a5829a44c0cfc5d778ddb56
Sashlemekhov <67118539+Sashlemekhov@users.noreply.github.com>
2020-11-28 00:05:34 +0200

    add files with HW

---
+++ gallery.js
@@ -0,0 +1,118 @@
+import galleryDataArr from './gallery-items.js';
+
+const galleryListRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
+const lightboxBtnCloseRef = document.querySelector('.lightbox__button');
+const lightboxImgRef = document.querySelector('.lightbox__image');
+
+galleryListRef.addEventListener('click', openModalHandler);
+lightboxOverlayRef.addEventListener('click', backdropClickHandler);
+lightboxBtnCloseRef.addEventListener('click', closeModalByClickOnBtn);
+
+// create Gallery Markup and add in DOM
+function createGalleryMarkup(galleryDataArr) {
+  return galleryDataArr.map(({preview, original, description, id}) => {
+    return `
+      <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+        >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          data-id="${id}"
+          alt="${description}"
+        />
+        </a>
+      </li>
+    `
+  }
+  ).join('');
+};
+galleryListRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryDataArr));
+
+// open modal
+function openModalHandler(event) {
+  event.preventDefault();
+  window.addEventListener('keydown', closeModalByEsc);
+  window.addEventListener('keydown', keyboardNavigationHandler);
+
+  const largeImgURL = event.target.dataset.source;
+  const largeImgAlt = event.target.getAttribute('alt');
+  const largeImgId = event.target.dataset.id;
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+
+  lightboxRef.classList.add('is-open');
+  lightboxImgRef.setAttribute('src', largeImgURL);
+  lightboxImgRef.setAttribute('alt', largeImgAlt);
+  lightboxImgRef.setAttribute('data-id', largeImgId);
+};
+
+//close modal
+function closeModalHandler() {
+  window.removeEventListener('keydown', closeModalByEsc);
+  window.removeEventListener('keydown', keyboardNavigationHandler);
+
+  lightboxImgRef.setAttribute('src', '');
+  lightboxRef.classList.remove('is-open');
+};
+
+//close modal by click on backdrop
+function backdropClickHandler(event) {
+  if (event.target === event.currentTarget) {
+    closeModalHandler();
+  };
+};
+
+//close modal by click on close button
+function closeModalByClickOnBtn(event)
+{
+  closeModalHandler();
+};
+
+//close modal by Esc
+function closeModalByEsc(event) {
+  if (event.code === 'Escape') {
+    closeModalHandler();
+  };
+};
+
+//gallery big imaga navigation
+function keyboardNavigationHandler(event) {
+  let prevImgNumber = Number(lightboxImgRef.dataset.id) - 1;
+  let nextImgNumber = Number(lightboxImgRef.dataset.id) + 1;
+  let attributeURL = (indexOfArr) => lightboxImgRef.setAttribute('src', galleryDataArr[indexOfArr].original);
+  let attributeAlt = (indexOfArr) => lightboxImgRef.setAttribute('alt', galleryDataArr[indexOfArr].description);
+  let attributeId = (indexOfArr) => lightboxImgRef.setAttribute('data-id', galleryDataArr[indexOfArr].id);
+
+  if (nextImgNumber === galleryDataArr.length) {
+    attributeURL(0);
+    attributeAlt(0);
+    attributeId(0);
+  };
+
+  if (prevImgNumber === -1) {
+    attributeURL(galleryDataArr.length - 1);
+    attributeAlt(galleryDataArr.length - 1);
+    attributeId(galleryDataArr.length - 1);
+  };
+  
+  if (event.code === 'KeyA' || event.code === 'ArrowLeft' || event.code === 'Numpad4') {
+    attributeURL(prevImgNumber);
+    attributeAlt(prevImgNumber);
+    attributeId(prevImgNumber);
+  };
+
+  if (event.code === 'KeyD' || event.code === 'ArrowRight' || event.code === 'Numpad6') {
+    attributeURL(nextImgNumber);
+    attributeAlt(nextImgNumber);
+    attributeId(nextImgNumber);
+  };
+};
+ 
